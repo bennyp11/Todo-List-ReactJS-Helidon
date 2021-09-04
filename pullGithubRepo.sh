863e528c7c2818ad194b403334db1fac31b89b36
bennyp11 <71295935+bennyp11@users.noreply.github.com>
2021-09-03 17:15:52 -0700

    Delete pullGithubRepo.sh

--- pullGithubRepo.sh
+++
@@ -1,9 +0,0 @@
-## MyToDoReact Dev environment setup
-## By Ben Pollock, Oracle Cloud
-
-git clone https://github.com/oracle/oci-react-samples.git 
-cd ~/oci-react-samples
-mv mtdrworkshop ..
-cd ..
-cd mtdrworkshop
-echo ****CONGRATS, YOU HAVE PULLED THE SAMPLES FROM GITHUB******
